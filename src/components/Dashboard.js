7742b37ceeebe838b61c20253cdb3f468c76b496
nicohsfu <marcnicohernandez@gmail.com>
2022-11-10 14:31:15 -0800

    binding this

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -33,9 +33,9 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-      focused: id
-    });
+    this.setState(previousState => ({
+      focused: previousState.focused !== null ? null : id
+    }));
   }
 
   render() {
@@ -51,10 +51,9 @@ class Dashboard extends Component {
       .map(panel => (
         <Panel
           key={panel.id}
-          id={panel.id}
           label={panel.label}
           value={panel.value}
-          onSelect={this.selectPanel}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
